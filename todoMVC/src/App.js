442dfdcf1dbc662ca7e220df463dc9426b33cf1b
zoujinqiang <jim@zoujinqiangdeMacBook-Pro.local>
2021-06-01 10:27:02 +0800

    dependency

--- todoMVC/src/App.js
+++ todoMVC/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
-// import State from './state/indexWithLocal'
+import State from './state/indexWithLocal'
 // import Hook from './hook/index'
 // import Redux from './redux/index'
 // import Router from './router'
-import Todo from './state/index'
+// import Todo from './state/index'
 
 function App() {
   return (
     <div>
-      {/* <State /> */}
+      <State />
       {/* <Hook /> */}
       {/* <Redux /> */}
-      <Todo />
+      {/* <Todo /> */}
     </div>
   );
 }
